bd92092a0f42597c521b42086647b733b3ddbb6d
Easton Lee <me@eastonlee.com>
2018-03-13 22:49:32 -0700

    780

---
+++ 780. Reaching Points.py
@@ -0,0 +1,119 @@
+import unittest
+from pprint import pprint
+import re
+import cProfile
+import heapq
+import bisect
+import collections
+import itertools
+import numpy as np
+
+'''
+
+'''
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return 'TreeNode({})'.format(self.val)
+
+
+def tree_deserialize(string):
+    """
+    author: @StefanPochmann
+    """
+    if string == '{}':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val))
+             for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
+    return root
+
+
+def tree_height(root):
+    """
+    author: @StefanPochmann
+    """
+    return 1 + max(tree_height(root.left), tree_height(root.right)) if root else -1
+
+
+def tree_draw(root):
+    def jumpto(x, y):
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+    def draw(node, x, y, dx):
+        if node:
+            t.goto(x, y)
+            jumpto(x, y - 20)
+            t.write(node.val, align='center', font=('Arial', 12, 'normal'))
+            draw(node.left, x - dx, y - 60, dx / 2)
+            jumpto(x, y - 20)
+            draw(node.right, x + dx, y - 60, dx / 2)
+    import turtle
+    t = turtle.Turtle()
+    t.speed(0)
+    turtle.delay(0)
+    h = tree_height(root)
+    jumpto(0, 30 * h)
+    draw(root, 0, 30 * h, 40 * h)
+    t.hideturtle()
+    turtle.mainloop()
+
+
+class Solution(object):
+
+    # https://leetcode.com/problems/reaching-points/solution/
+    def reachingPoints(self, sx, sy, tx, ty):
+        """
+        :type sx: int
+        :type sy: int
+        :type tx: int
+        :type ty: int
+        :rtype: bool
+        """
+        while tx >= sx and ty >= sy:
+            if tx == ty:
+                break
+            elif tx > ty:
+                if ty > sy:
+                    tx %= ty
+                else:
+                    return (tx - sx) % ty == 0
+            else:
+                if tx > sx:
+                    ty %= tx
+                else:
+                    return (ty - sy) % tx == 0
+
+        return tx == sx and ty == sy
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        sx = 1; sy = 1; tx = 3; ty = 5
+        assert Solution().reachingPoints(sx, sy, tx, ty) == True
+        sx = 1; sy = 1; tx = 2; ty = 2
+        assert Solution().reachingPoints(sx, sy, tx, ty) == False
+        sx = 1; sy = 1; tx = 1; ty = 1
+        assert Solution().reachingPoints(sx, sy, tx, ty) == True
+        #cProfile.runctx('Solution().calculate(case)', globals(), locals(), sort='cumtime')
+
+    def est_tree_draw(self):
+        tree_draw(tree_deserialize('[1,2,3,4,5,6,7]'))
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
