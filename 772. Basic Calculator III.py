bc560ada7b379d23145f57fcd967d13f558612ac
Easton Lee <me@eastonlee.com>
2018-03-22 16:54:41 -0700

    772, 803

---
+++ 772. Basic Calculator III.py
@@ -0,0 +1,156 @@
+import bisect
+import collections
+import cProfile
+import heapq
+import itertools
+import math
+import re
+import unittest
+from pprint import pprint
+
+import numpy as np
+
+'''
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+
+The expression string contains only non-negative integers, +, -, *, / operators , open ( and closing parentheses ) and empty spaces . The integer division should truncate toward zero.
+
+You may assume that the given expression is always valid.
+
+Some examples:
+
+"1 + 1" = 2
+" 6-4 / 2 " = 4
+"2*(5+5*2)/3+(6/2+8)" = 21
+"(2+6* 3+5- (3*14/7+2)*5)+3"=-12
+ 
+
+Note: Do not use the eval built-in library function.
+'''
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return 'TreeNode({})'.format(self.val)
+
+
+def tree_deserialize(string):
+    """
+    author: @StefanPochmann
+    """
+    if string == '{}':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val))
+             for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
+    return root
+
+
+def tree_height(root):
+    """
+    author: @StefanPochmann
+    """
+    return 1 + max(tree_height(root.left), tree_height(root.right)) if root else -1
+
+
+def tree_draw(root):
+    def jumpto(x, y):
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+    def draw(node, x, y, dx):
+        if node:
+            t.goto(x, y)
+            jumpto(x, y - 20)
+            t.write(node.val, align='center', font=('Arial', 12, 'normal'))
+            draw(node.left, x - dx, y - 60, dx / 2)
+            jumpto(x, y - 20)
+            draw(node.right, x + dx, y - 60, dx / 2)
+    import turtle
+    t = turtle.Turtle()
+    t.speed(0)
+    turtle.delay(0)
+    h = tree_height(root)
+    jumpto(0, 30 * h)
+    draw(root, 0, 30 * h, 40 * h)
+    t.hideturtle()
+    turtle.mainloop()
+
+
+class Solution(object):
+
+    # https://github.com/kamyu104/LeetCode/blob/master/Python/basic-calculator-iii.py
+    # This problem is only open to premium members
+    def calculate(self, s):
+        operands, operators = [], []
+        operand = ""
+        for i in reversed(range(len(s))):
+            if s[i].isdigit():
+                operand += s[i]
+                if i == 0  or not s[i-1].isdigit():
+                    operands.append(int(operand[::-1]))
+                    operand = ""
+            elif s[i] == ')' or s[i] == '*' or s[i] == '/':
+                operators.append(s[i])
+            elif s[i] == '+' or s[i] == '-':
+                while operators and \
+                      (operators[-1] == '*' or operators[-1] == '/'):
+                    self.compute(operands, operators)
+                operators.append(s[i])
+            elif s[i] == '(':
+                while operators[-1] != ')':
+                    self.compute(operands, operators)
+                operators.pop()
+                
+        while operators:
+            self.compute(operands, operators)
+            
+        return operands[-1]
+
+    def compute(self, operands, operators):
+        left, right = operands.pop(), operands.pop()
+        op = operators.pop()
+        if op == '+':
+            operands.append(left + right)
+        elif op == '-':
+            operands.append(left - right)
+        elif op == '*':
+            operands.append(left * right)
+        elif op == '/':
+            operands.append(left / right)
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        case = "1 + 1"
+        assert Solution().calculate(case) == 2
+        case = " 6-4 / 2 "
+        assert Solution().calculate(case) == 4
+        case = "2*(5+5*2)/3+(6/2+8)"
+        assert Solution().calculate(case) == 21
+        case = "(2+6* 3+5- (3*14/7+2)*5)+3"
+        assert Solution().calculate(case) == -12
+        #cProfile.runctx('Solution().calculate(case)', globals(), locals(), sort='cumtime')
+
+    def est_tree_draw(self):
+        tree_draw(tree_deserialize('[1,2,3,4,5,6,7]'))
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+
