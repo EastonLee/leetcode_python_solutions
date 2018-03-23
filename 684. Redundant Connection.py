a985e03ed46957ec0ec5ca6bf583f563f47d7e81
Easton Lee <me@eastonlee.com>
2018-03-23 15:55:45 -0700

    684

---
+++ 684. Redundant Connection.py
@@ -0,0 +1,130 @@
+import bisect
+import collections
+import cProfile
+import heapq
+import itertools
+import math
+import re
+import unittest
+from pprint import pprint
+
+import numpy as np
+
+'''
+
+'''
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return 'TreeNode({})'.format(self.val)
+
+
+def tree_deserialize(string):
+    """
+    author: @StefanPochmann
+    """
+    if string == '{}':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val))
+             for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
+    return root
+
+
+def tree_height(root):
+    """
+    author: @StefanPochmann
+    """
+    return 1 + max(tree_height(root.left), tree_height(root.right)) if root else -1
+
+
+def tree_draw(root):
+    def jumpto(x, y):
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+    def draw(node, x, y, dx):
+        if node:
+            t.goto(x, y)
+            jumpto(x, y - 20)
+            t.write(node.val, align='center', font=('Arial', 12, 'normal'))
+            draw(node.left, x - dx, y - 60, dx / 2)
+            jumpto(x, y - 20)
+            draw(node.right, x + dx, y - 60, dx / 2)
+    import turtle
+    t = turtle.Turtle()
+    t.speed(0)
+    turtle.delay(0)
+    h = tree_height(root)
+    jumpto(0, 30 * h)
+    draw(root, 0, 30 * h, 40 * h)
+    t.hideturtle()
+    turtle.mainloop()
+
+class DSU(object):
+    def __init__(self):
+        self.par = list(range(1001))
+        self.rnk = [0] * 1001
+
+    def find(self, x):
+        if self.par[x] != x:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+
+    def union(self, x, y):
+        xr, yr = self.find(x), self.find(y)
+        if xr == yr:
+            return False
+        elif self.rnk[xr] < self.rnk[yr]:
+            self.par[xr] = yr
+        elif self.rnk[xr] > self.rnk[yr]:
+            self.par[yr] = xr
+        else:
+            self.par[yr] = xr
+            self.rnk[xr] += 1
+        return True
+
+class Solution(object):
+    # https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+    # https://leetcode.com/articles/redundant-connection/
+    def findRedundantConnection(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        dsu = DSU()
+        for edge in edges:
+            if not dsu.union(*edge):
+                return edge
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        case = [[1,5],[3,4],[3,5],[4,5],[2,4]]
+        assert Solution().findRedundantConnection(case) == [4,5]
+        case = [[1,2], [1,3], [2,3]]
+        assert Solution().findRedundantConnection(case) == [2,3]
+        case = [[1,2], [2,3], [3,4], [1,4], [1,5]]
+        assert Solution().findRedundantConnection(case) == [1,4]
+        #cProfile.runctx('Solution().calculate(case)', globals(), locals(), sort='cumtime')
+
+    def est_tree_draw(self):
+        tree_draw(tree_deserialize('[1,2,3,4,5,6,7]'))
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+
