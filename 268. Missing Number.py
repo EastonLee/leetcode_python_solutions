b47b55c9823f72121c9553604dddc80061fd689d
Easton Lee <easton.lee.cs@gmail.com>
2022-05-29 17:35:25 +0800

    268

---
+++ 268. Missing Number.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = [
+            ["1+7-(7+3+3)+6-3+1", -1]
+        ]
+        for ci, co in cases:
+            result = Solution().calculate(ci)
+            assert result == co, (ci, co, result)
+        # cProfile.runctx('Solution().calculate(case)', globals(), locals(), sort='cumtime')
+
+    def est_tree_draw(self):
+        tree_draw(tree_deserialize('[1,2,3,4,5,6,7]'))
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
